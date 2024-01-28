743ea0b3c024a11f7beb0f5c83bd77cbb1d2686a
Konstantin <119262845+Yodakole1@users.noreply.github.com>
2024-01-28 21:02:23 +0100

    Update Fizzbuzz_c++.cpp

--- Fizzbuzz_c++.cpp
+++ Fizzbuzz_c++.cpp
@@ -8,7 +8,7 @@ using namespace std;
 vector<string> metod(int num,vector<string>resenje){
     for (int i = 1; i < num; i++)
     {
-        if (i%5==0 && i%3==0)
+        if (i%15==0)
         {
             resenje.push_back("FizzBuzz");
         }   else if (i%3==0)
