2b3d18c29240cd918ba7f0aecb58847e9d73de3f
Konstantin <119262845+Yodakole1@users.noreply.github.com>
2024-01-28 21:02:50 +0100

    Update Fizzbuzz_python.py

--- Fizzbuzz_python.py
+++ Fizzbuzz_python.py
@@ -6,7 +6,7 @@ resenje = []
 
 def metod(num):
     for i in range(1, num):
-        if(i%3==0 and i%5==0):
+        if(i%15==0):
             resenje.append("FizzBuzz")
         elif(i%3==0):
             resenje.append("Fizz")
